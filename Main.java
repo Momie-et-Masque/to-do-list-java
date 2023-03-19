0e612c1d1361a4f290429ee344b3294fcec9a558
Momie-et-Masque <127416928+Momie-et-Masque@users.noreply.github.com>
2023-03-19 11:15:02 +0100

    update (more commands)

--- Main.java
+++ Main.java
@@ -2,33 +2,72 @@ import java.util.ArrayList; // Import the ArrayList class for dynamically resiza
 import java.util.Scanner;  // Import the Scanner class for user input
 
 public class Main {
-  static ArrayList<String> tasks = new ArrayList<String>(); // Create an ArrayList object
-  static Scanner userInput = new Scanner(System.in);  // Create a Scanner object
-  static String input; // Initialize the input variable
-  static String[] splitInput; // Initialize the splitInput variable
-  static String command; // Initialize the command variable
+  static Scanner userInput = new Scanner(System.in); // module for user input
+  
+  static ArrayList<String> tasks = new ArrayList<String>();
+  static ArrayList<Boolean> done = new ArrayList<Boolean>();  
+  static ArrayList<Boolean> important = new ArrayList<Boolean>();  
+  static String input;
+  static String[] splitInput;
+  
+    // Color constants
+  static final String RESET = "\u001B[0m";
+  static final String RED = "\u001B[31m";
+  static final String GREEN = "\u001B[32m";
+  static final String YELLOW = "\u001B[33m";
+  static final String BLUE = "\u001B[34m";
+  static final String MAGENTA = "\u001B[35m";
+  static final String CYAN = "\u001B[36m";
+  static final String WHITE = "\u001B[37m";
   
   public static void main(String[] args) {
     while (true) {
       System.out.print("\033[H\033[2J"); // cls
       System.out.println("###################\n#                 #\n#  MY TO-DO LIST  #\n#                 #\n###################\n"); // stylized title 
       for (int i = 0; i < tasks.size(); i++) {
-        System.out.println(tasks.get(i));
+        if (important.get(i)) {
+          System.out.print(RED);
+        }
+        if (done.get(i)) {
+          System.out.print("[X] " + tasks.get(i));
+        } else {
+          System.out.print("[ ] " + tasks.get(i));
+        }
+        System.out.println(RESET);
       }
       
-      input = userInput.nextLine(); // Read user imput
+      input = userInput.nextLine( ); // Read user imput
       
       if (input.startsWith("/")){
-        splitInput = input.split(" ");
-        switch (splitInput[0]) {
-          case "/add":
-            appendTask(lastArg(1));
-            break;
-          case "/del":
-            deleteTask(splitInput[1]);
-            break;
-          default:
-            appendTask(input);
+        try {
+          splitInput = input.split(" ");
+          switch (splitInput[0]) {
+            case "/add":
+              appendTask(lastArg(1));
+              break;
+            case "/insert":
+              insertTask(splitInput[1], lastArg(2));
+              break;
+            case "/del":
+              deleteTask(splitInput[1]);
+              break;
+            case "/edit":
+              editTask(splitInput[1], lastArg(2));
+              break;
+            case "/done":
+              doneTask(splitInput[1]);
+              break;
+            case "/!":
+              importantTask(splitInput[1]);
+              break;
+            case "/clear":
+              clearTasks();
+              break;
+            default:
+              appendTask(input);
+          }
+        } catch (Exception e) {
+          appendTask(input);
         }
       } else {
         appendTask(input);
@@ -38,18 +77,47 @@ public class Main {
   
   static void appendTask(String task) {
     tasks.add(task);
+    done.add(false);
+    important.add(false);
   }
-  static void editTask(String task) {
-    tasks.add(task);
+  static void insertTask(String indexStr, String task) {
+    int index = Integer.parseInt(indexStr) - 1;
+    tasks.add(index, task);
+    done.add(index, false);
+    important.add(index, false);
+  }
+  static void editTask(String indexStr, String newTask) {
+    int index = Integer.parseInt(indexStr);
+    tasks.set(index - 1, newTask);
   }
   static void deleteTask(String indexStr) {
-    try {
-      int index = Integer.parseInt(indexStr);
-      tasks.remove(index - 1);
-    } catch (Exception e) {
-      // error : argument cannot be converted into int or index given is incorrect
+    int index = Integer.parseInt(indexStr);
+    tasks.remove(index - 1);
+    done.remove(index - 1);
+    important.remove(index - 1);
+  }
+  static void doneTask(String indexStr) {
+    int index = Integer.parseInt(indexStr) - 1;
+    if (done.get(index)) {
+      done.set(index, false);
+    } else {
+      done.set(index, true);
     }
   }
+  static void importantTask(String indexStr) {
+    int index = Integer.parseInt(indexStr) - 1;
+    if (important.get(index)) {
+      important.set(index, false);
+    } else {
+      important.set(index, true);
+    }
+  }
+  static void clearTasks() {
+    tasks.clear();
+    done.clear();
+    important.clear();
+  }
+  
   static String lastArg(int index) {
     int startIndex = 0;
     for (int i = 0; i < index; i++) {
--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # to-do-list-java
-This program is a simple command-line to-do list application in Java that allows the user to add, edit and delete tasks.
+This program is a simple command-line to-do list application in Java that allows the user to manipulate the list and its items.
 
-The program uses an ArrayList object to store the tasks and a Scanner object to read user input from the command line.
+Input a task to append it to the list. You can also input slash-commands.
 
-The main method uses a while loop to continuously prompt the user for input and display the current list of tasks. If the user enters a command (starting with a forward slash), the program parses the command and calls the appropriate method (appendTask, editTask, or deleteTask) with the appropriate arguments. If the user enters a regular task (not starting with a forward slash), the program adds the task to the ArrayList using the appendTask method.
+Commands :
 
-The appendTask method simply adds a task to the end of the ArrayList.
+`/add {task}` : appends the task to the list
+`/insert {index} {task}` : inserts the task to the given index
+`/del {index}` : deletes the task at the given index
+`/done {index}` : marks or unmarks the task at the given index as done
+`/! {index}` : marks or unmarks the task at the given index as important
+`/clear` : clears the entire list
 
-The editTask method is not currently used in the program, so it can be removed.
-
-The deleteTask method attempts to parse the indexStr argument as an integer and removes the task at that index (adjusted for zero-based indexing) from the ArrayList. If an exception is thrown (indicating that the argument was not an integer), the program simply ignores the command and continues.
-
-The lastArg method is a utility method that extracts the last argument from a command string (split by spaces). It is used to extract the task description from the "/add" command.
-
-Overall, this program provides a basic framework for a to-do list application, but it is missing several important features such as saving tasks between sessions and providing more advanced editing capabilities.
+Note : indexes in commands start from 1
